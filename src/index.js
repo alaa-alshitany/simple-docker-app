c359941bc8132a95134defa75582f2c602fe10a2
alaaWalid <alaa2alshitany@gmail.com>
2024-02-22 21:40:54 +0200

    Added mongo-db variables

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@
 // import express package
 const express = require('express');
 
+//import mongoose package
+const mongoose = require('mongoose');
+
+//connect to db
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo';
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(URI).then(() => console.log('connect to db ...')).catch((err) => console.log('failed to connect',err));
+
 //specify the port that the app will run on 
 const PORT = 4000;
 
